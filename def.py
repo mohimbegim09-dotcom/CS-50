975e101166383e2500a54fbd7d9362666ff0c109
Mohimbegim <mohimbegim09@gmail.com>
2026-07-26 18:54:52 +0500

    new file

---
+++ def.py
@@ -0,0 +1,21 @@
+def hello():
+    print('hello')
+
+
+name = input("Enter your name: ")
+hello()
+print(name)
+
+#OR
+def hello(to):
+    print('hello,', to)
+
+name = input("Enter your name: ")
+hello(name)
+
+# But if you dont want to put name into hello() function, 
+#you can assign any other meaning, using this:
+def hello(to="world"):
+    print('hello,', to)
+
+hello() # will print hello, world
